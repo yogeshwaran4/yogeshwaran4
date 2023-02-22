eb761a7ac5cf7b91b1f936f70aa9cbe1e0790ea1
Yogeshwaran P <85277785+yogeshwaran4@users.noreply.github.com>
2023-02-22 17:31:12 +0530

    Update DBconnection.java

--- FeeManagement/src/main/java/com/yogesh/database/DBconnection.java
+++ FeeManagement/src/main/java/com/yogesh/database/DBconnection.java
@@ -3,6 +3,8 @@ package com.yogesh.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+//Add database in mysql before get the connection
+
 public class DBconnection {
 	static String url = "jdbc:mysql://localhost:3306/feemanagement";
 	static String usname = "root";
